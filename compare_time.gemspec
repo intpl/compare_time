7baa30b2b6dc1a3f80d7b6a6ea4d159861a2289d
Bartek Gladecki <bgladecki@gmail.com>
2016-08-28 17:15:08 +0200

    init

---
+++ compare_time.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'CompareTime'
+  s.version     = '0.0.1'
+  s.date        = '2016-08-28'
+  s.summary     = "Compare execution times for given blocks"
+  s.description = "Compare execution times for given blocks, right?"
+  s.authors     = ["Bartek Gladecki"]
+  s.email       = 'bgladecki@gmail.com'
+  s.files       = ["lib/compare_time.rb"]
+  s.homepage    = 'http://rubygems.org/gems/compare_time'
+  s.license     = 'MIT'
+end
---
+++ lib/compare_time.rb
@@ -0,0 +1,46 @@
+require 'benchmark'
+require 'colorize'
+
+# CompareTime.new.compare(:whatever_name_you_want) {
+#  ...
+# }.with(:whatever_name_you_want2) {
+#  ...
+# }.print_results
+
+class CompareTime
+  attr_reader :benchmarks
+
+  def initialize()
+    # add how many times
+    @benchmarks = {}
+  end
+
+  def compare(symbol, &block)
+    execute_and_save(symbol, block)
+    self
+  end
+
+  def results
+    @benchmarks.sort_by(&:last).map do |arr|
+      "#{arr[0]}: #{'%.10f' % arr[1]}"
+    end
+  end
+
+  def print_results
+    puts results[0].colorize(:green)
+
+    results.drop(1).each do |res|
+      puts res
+    end
+  end
+
+  alias_method :with, :compare
+
+  private def execute_and_save(symbol, block)
+    original_stdout = $stdout
+    $stdout = StringIO.new
+    @benchmarks[symbol] = Benchmark.realtime(&block)
+  ensure
+    $stdout = original_stdout
+  end
+end
